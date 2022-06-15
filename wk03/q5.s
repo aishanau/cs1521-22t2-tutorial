133bf35c0c09f16bc84f1f0c51a0710cde0bdaee
Aisha Nauman <aishanauman01@gmail.com>
2022-06-15 17:21:04 +1000

    pushing completed code

--- wk03/q5.s
+++ wk03/q5.s
@@ -1,7 +1,48 @@
 main:
 
+	li	$t0, 0
+loop:
+	bge	$t0, 10, end
 
+	li	$v0, 5
+	syscall
+	move	$t1, $v0		# $t1 holds scanf value
 
+	la	$t2, numbers		# offset of the array
+	mul	$t3, $t0, 4		# i * 4 (which is the size of each element in the array)
+	add	$t3, $t3, $t2		# offset + i * 4
 
-    li $v0, 0                   # return 0
-    jr $ra
+	sw	$t1, ($t3)		# store value of t1 into address thats held in t3
+
+	add	$t0, $t0, 1
+	j	loop
+end:
+
+	# print out array 
+
+	li	$t0, 0
+loop_print:	
+	bge	$t0, 10, end_loop_print
+
+	la	$t2, numbers
+	mul	$t3, $t0, 4
+	add	$t3, $t3, $t2
+
+	lw	$a0, ($t3)
+
+	# print out syscall
+	li	$v0, 1
+	syscall
+
+	add	$t0, $t0, 1
+	j	loop_print
+
+end_loop_print:
+
+	li $v0, 0					# return 0
+	jr $ra
+
+.data
+
+numbers:
+	.word 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
--- wk03/q7.s
+++ wk03/q7.s
@@ -1,7 +1,48 @@
 main:
 
+	li	$t0, 0
+loop:
+	bge	$t0, 10, end
+
+	la	$t2, numbers		# offset of the array
+	mul	$t3, $t0, 4		# i * 4 (which is the size of each element in the array)
+	add	$t3, $t3, $t2		# offset + i * 4
+
+	lw	$t1, ($t3)		# get value of numbers[i] into t1
+	
+	bge	$t1, 0, loop_increment
+
+	add	$t1, $t1, 42		# numbers[i] + 42
+	sw	$t1, ($t3)		# numbers[i] = numbers[i] + 42
+
+loop_increment:
+	add	$t0, $t0, 1
+	j	loop
+end:
 
 
+	li	$t0, 0
+loop_print:
+	bge	$t0, 10, end_print
 
-    li $v0, 0                   # return 0
-    jr $ra
+	mul	$t3, $t0, 4    #   calculate &numbers[i]
+	la	$t2, numbers   #
+	add	$t3, $t3, $t2  #
+	lw	$a0, ($t3)     #   load numbers[i] into $a0
+	li	$v0, 1         #   printf("%d", numbers[i])
+	syscall
+
+	li	$a0, '\n'      #   printf("%c", '\n');
+	li	$v0, 11
+	syscall
+
+	add	$t0, $t0, 1
+	j	loop_print
+end_print:
+	li $v0, 0					# return 0
+	jr $ra
+
+.data
+
+numbers:
+	.word 0, 1, 2, -3, 4, -5, 6, -7, 8, 9

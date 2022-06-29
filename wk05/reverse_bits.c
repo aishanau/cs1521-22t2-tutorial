877a06988a9811e90ecc1bd69ad2b42b9644a24e
Aisha Nauman <aishanauman01@gmail.com>
2022-06-29 17:09:08 +1000

    pushing tut code

--- wk05/reverse_bits.c
+++ wk05/reverse_bits.c
@@ -1,10 +1,29 @@
 #include <stdio.h>
 #include <assert.h>
+#include <stdint.h>
 
-typedef unsigned int Word;
+typedef uint64_t Word;
+
+#define SIZE_OF_INT 64
+#define BASE_MASK 1u // 1u << 63 
 
 Word reverseBits(Word w) {
-    return w;
+    // 1. loop over all 32 bits 
+    Word reversed_number = 0;
+
+    for (Word bit = 0; bit < SIZE_OF_INT; bit++) {
+        // declare our masks here 
+        // mask to check 
+        Word check_mask = BASE_MASK << (SIZE_OF_INT - 1 - bit);
+        // mask to set return value
+        Word return_mask = BASE_MASK << bit;
+        // check if bit is set (check if 1 or 0)
+        // if bit is 1, then set the bit in the opposite direction 
+        if (check_mask & w) { // if statement is always true if the return value is nonzero 
+            reversed_number = reversed_number | return_mask;
+        }
+    }
+    return reversed_number;
 }
 
 // testing
